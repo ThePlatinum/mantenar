0aac98ada9ee32661a8781a205b53ce1076009fc
Platinum <platinumemirate@gmail.com>
2022-10-29 18:00:31 +0100

    alert and favicon

--- resources/views/layouts/theme.blade.php
+++ resources/views/layouts/theme.blade.php
@@ -12,6 +12,22 @@
 <body>
 
   <main class="py-4 container">
+
+    @if(session()->has('success'))
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+      <i class='bx bx-check-circle pe-1'></i>
+      {{ session()->get('success') }}
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    @endif
+    @if(session()->has('error'))
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+      <i class='bx bx-error-circle pe-1'></i>
+      {{ session()->get('error') }}
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    @endif
+
     @yield('content')
 
     <div class="text-center py-4">
@@ -24,12 +40,17 @@
   <link href="{{ asset('util/multi-select/filter_multi_select.css') }}" rel="stylesheet">
   <link href="{{ asset('util/bootstrap-5.1.3-slim/bootstrap.min.css') }}" rel="stylesheet">
 
-  <script src="{{ asset('util/bootstrap-5.1.3-slim/bootstrap.min.js') }}" ></script>
-  <script src="{{ asset('util/jquery-3.6.0.js') }}" ></script>
-  <script src="{{ asset('util/multi-select/filter-multi-select-bundle.min.js') }}" ></script>
-  <script src="{{ asset('util/bootbox.min.js') }}" ></script>
-  
+  <script src="{{ asset('util/bootstrap-5.1.3-slim/bootstrap.min.js') }}"></script>
+  <script src="{{ asset('util/jquery-3.6.0.js') }}"></script>
+  <script src="{{ asset('util/multi-select/filter-multi-select-bundle.min.js') }}"></script>
+  <script src="{{ asset('util/bootbox.min.js') }}"></script>
+
   <link href="{{ asset('util/custom.css') }}" rel="stylesheet">
+  <script>
+    setTimeout(function() {
+      $(".alert").hide('medium');
+    }, 1000);
+  </script>
   @stack('scripts')
 </body>
 
