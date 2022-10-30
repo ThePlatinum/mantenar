99e4c83aaed0f5ab7a5d4d7cc050e9bdbeb64cff
Platinum <platinumemirate@gmail.com>
2022-10-30 22:19:40 +0100

    Auth UIs

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,55 @@
-@extends('layouts.app')
+@extends('layouts.theme')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+<div class="row">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+  <div class="col-md-2"></div>
 
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
+  <div class="col-2">
+    <div class="bg__blue p-4 r__10 h-100"></div>
+  </div>
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+  <div class="col-10 col-md-6 py-5">
+    <img src="{{ asset('images/mantenar_logo.svg') }}" alt="Mantenar Logo">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+    <div class="bg-white ml__neg__ p-4 pe-5 mt-4 r__10">
 
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+      <div class="pb-4">
+        <small class="text-muted">Organization</small>
+        <h1> {{ \App\Models\Setting::org_name() ?? '' }} </h1>
+      </div>
+
+      <h4>Reset Password</h4>
+      <form method="POST" action="{{ route('password.email') }}">
+        @csrf
+
+        <div class="row mb-3">
+          <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+
+          <div class="col-md-6">
+            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+            @error('email')
+            <span class="invalid-feedback" role="alert">
+              <strong>{{ $message }}</strong>
+            </span>
+            @enderror
+          </div>
         </div>
+
+        <div class="row mb-0">
+          <div class="col-md-6 offset-md-4">
+            <button type="submit" class="btn btn-primary">
+              {{ __('Send Password Reset Link') }}
+            </button>
+          </div>
+        </div>
+      </form>
     </div>
+  </div>
+
+  <div class="col-md-2"></div>
+
 </div>
 @endsection
--- resources/views/layouts/theme.blade.php
+++ resources/views/layouts/theme.blade.php
@@ -20,6 +20,13 @@
       <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
     @endif
+    @if (session('status'))
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+      <i class='bx bx-check-circle pe-1'></i>
+      {{ session('status') }}
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    @endif
     @if(session()->has('error'))
     <div class="alert alert-danger alert-dismissible fade show" role="alert">
       <i class='bx bx-error-circle pe-1'></i>
