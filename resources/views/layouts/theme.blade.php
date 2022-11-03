045dd1bfbed55fcddd6f7f98064aef2e5608d0d8
Platinum <platinumemirate@gmail.com>
2022-11-03 19:34:02 +0100

    add laravel-websocket

--- resources/views/layouts/theme.blade.php
+++ resources/views/layouts/theme.blade.php
@@ -6,8 +6,10 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>{{ config('app.name', 'Mantenar') }}</title>
-  <meta name="author" content="Emmanuel Adesina"></meta>
-  <meta name="description" content="Software  to keep organization file share secure and unbounded"></meta>
+  <meta name="author" content="Emmanuel Adesina">
+  </meta>
+  <meta name="description" content="Software  to keep organization file share secure and unbounded">
+  </meta>
   <link rel="icon" type="images/mantenar.svg" href="{{ asset('images/mantenar.svg') }}">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" defer>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous" defer></script>
@@ -61,6 +63,19 @@
     setTimeout(function() {
       $(".alert").hide('medium');
     }, 3000);
+
+    // Notification 
+    if (!("Notification" in window))
+      alert("This browser does not support desktop notification");
+    else if (Notification.permission === "granted") {
+      const notification = new Notification("Hi there!");
+    } else if (Notification.permission !== "denied") {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          const notification = new Notification("Hi there!");
+        }
+      });
+    }
   </script>
   @stack('scripts')
 </body>
